50d8cc2203fc77362dc341427e152c9e1e956c60
Simone Franchina <simone@italianonprofit.it>
2024-02-21 09:44:48 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <pre>
+    <code>
+       image loaded from "/public/image/sample.jpg"
+    </code>
+  </pre>
+  <img src="/images/sample.jpg" alt="" style="max-width: 100%">
+</template>
